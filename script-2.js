eb12cd907038aff2ad3356bc3955a9438144c7a8
Bryan Peens <peensbryan1@gmail.com>
2024-03-27 23:08:20 -0400

    created carousel component and updated script file

--- script-2.js
+++ script-2.js
@@ -1,78 +1,105 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let apiKey = 'UVHidgwWtNYfWtfAVtfjc2g4EHwmDOjuPkPmYhaL1VqdgneZ0D';
-    let pokemonImages = [];
+document.addEventListener("DOMContentLoaded", function () {
 
-    function getTumblr() {
-        let tag = "charizard";
-        let limit = 50; // Change the limit to the desired number of posts
-        let apiUrl = `https://api.tumblr.com/v2/tagged?tag=${tag}&limit=${limit}&api_key=${apiKey}&callback=?`;
+  let apiKey = "UVHidgwWtNYfWtfAVtfjc2g4EHwmDOjuPkPmYhaL1VqdgneZ0D";
+  let pokemonImages = [];
 
-        fetch(apiUrl)
-            .then(response => response.text())
-            .then(data => {
-                let match = data.match(/(?<=\().*(?=\))/);
-                if (match) {
-                    let jsonData = JSON.parse(match[0]);
-                    let posts = jsonData.response;
+  function getTumblr() {
+    let tag = "pokemongo";
+    let limit = 20; // Change the limit to the desired number of posts
+    let apiUrl = `https://api.tumblr.com/v2/tagged?tag=${tag}&limit=${limit}&api_key=${apiKey}`;
 
-                    for (let i = 0; i < posts.length; i++) {
-                        let post = posts[i];
-                        // Check if the post contains the tag "pokemon"
-                        if (post.tags.includes("pokemon", "pikachu", "charizard") ) {
-                            // Extract blog name, image URL, and entire post content
-                            let blogName = post.blog.name;
-                            let imageUrl = post.photos && post.photos[0].original_size.url; // Check if post.photos is defined
-                            let entirePostContent = post.body; // Change this line to get the entire post content
-                            pokemonImages.push({ blogName, imageUrl, entirePostContent });
-                        }
-                    }
+    fetch(apiUrl)
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data)
 
-                    // Call the display function
-                    displayPokemonImages();
-                } else {
-                    console.error("JSON object not found in response");
-                }
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-    }
+        let jsonData = JSON.parse(data);
 
-    function displayPokemonImages() {
-        let searchResultsElement = document.getElementById("searchResults");
-        if (searchResultsElement) {
-            searchResultsElement.textContent = ""; // Clear previous content
-            for (let i = 0; i < pokemonImages.length; i++) {
-                let pokemon = pokemonImages[i];
-                let postElement = document.createElement("div");
-                postElement.classList.add("bg-white", "rounded", "shadow", "p-4");
+        let posts = jsonData.response;
+          for (let i = 0; i < posts.length; i++) {
+            let post = posts[i];
+            // Check if the post contains the tag "pokemon"
+            // if (post.tags.includes("pokemongo", "pikachu", "charizard")) {
+              // Extract blog name, image URL, and entire post content
+              let blogName = post.blog.name;
+              let imageUrl = post.photos && post.photos[0].original_size.url; // Check if post.photos is defined
+              let entirePostContent = post.body; // Change this line to get the entire post content
+              pokemonImages.push({ blogName, imageUrl, entirePostContent });
+            // }
+          }
 
-                // Blog name
-                let titleElement = document.createElement("h3");
-                titleElement.textContent = pokemon.blogName;
-                titleElement.classList.add("font-semibold", "text-lg", "mb-2");
-                postElement.appendChild(titleElement);
+          displayPokemonImages();
+          initCarousel();
 
-                // Image
-                if (pokemon.imageUrl) { // Check if imageUrl is defined
-                    let imgElement = document.createElement("img");
-                    imgElement.className = "h-40 w-full object-cover";
-                    imgElement.src = pokemon.imageUrl;
-                    postElement.appendChild(imgElement);
-                }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-                // Entire post content
-                let contentElement = document.createElement("p");
-                contentElement.innerHTML = pokemon.entirePostContent; // Use innerHTML to render HTML content
-                contentElement.classList.add("text-sm");
-                postElement.appendChild(contentElement);
+  function displayPokemonImages() {
+    let searchResultsElement = document.getElementById("searchResults");
+    if (searchResultsElement) {
+      searchResultsElement.textContent = ""; // Clear previous content
+      for (let i = 0; i < pokemonImages.length; i++) {
+        let pokemon = pokemonImages[i];
+        let postElement = document.createElement("li");
+        postElement.classList.add("bg-white", "rounded", "shadow", "p-4", "overflow-y-scroll", "overflow-x-hidden", "max-h-64" );
 
-                searchResultsElement.appendChild(postElement);
-            }
-        } else {
-            console.error("searchResults element not found");
+        // Blog name
+        let titleElement = document.createElement("h3");
+        titleElement.textContent = pokemon.blogName;
+        titleElement.classList.add("font-semibold", "text-lg", "mb-2");
+        postElement.appendChild(titleElement);
+
+        // Image
+        if (pokemon.imageUrl) {
+          // Check if imageUrl is defined
+          let imgElement = document.createElement("img");
+          imgElement.className = "h-40 w-full object-cover";
+          imgElement.src = pokemon.imageUrl;
+          postElement.appendChild(imgElement);
         }
-    }
 
-    getTumblr(); // Call getTumblr() when the script is executed
+        // Entire post content
+        let contentElement = document.createElement("p");
+        contentElement.innerHTML = pokemon.entirePostContent; // Use innerHTML to render HTML content
+        contentElement.classList.add("text-sm");
+        postElement.appendChild(contentElement);
+
+        searchResultsElement.appendChild(postElement);
+      }
+    } else {
+      console.error("searchResults element not found");
+    }
+  }
+
+  getTumblr(); // Call getTumblr() when the script is executed
 });
+
+
+function initCarousel() {
+  var glide01 = new Glide(".glide-01", {
+    type: "carousel",
+    focusAt: "center",
+    perView: 5,
+    autoplay: 1200,
+    animationDuration: 700,
+    gap: 44,
+    classes: {
+      activeNav: "[&>*]:bg-slate-700",
+    },
+    breakpoints: {
+      1024: {
+        perView: 2,
+      },
+      640: {
+        perView: 1,
+      },
+    },
+  });
+
+  glide01.mount();
+}
+
+
